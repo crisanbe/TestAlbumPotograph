0d67d569dea4842782f17ecf7914c93186ee5fac
crisanbe289 <crisanbe289@gmail.com>
2023-10-08 03:54:53 -0500

    Se agrega splahscren modo dark y leght y icono de la app

--- app/src/main/java/com/cvelez/photos/application/MainActivity.kt
+++ app/src/main/java/com/cvelez/photos/application/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cvelez.photos.application
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -19,7 +20,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.nav_host_fragment)
